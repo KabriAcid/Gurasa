e191212d882655661e0673b2929ca1f6897b5ef4
Abdullahi Kabri <kabriacid01@gmail.com>
2024-12-25 02:39:25 +0100

    commit

---
+++ gurasa-app/public/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../components/Header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Welcome to Gurasa!</h2>
+        <p>
+          Order your favorite dishes online and have them delivered to your
+          doorstep.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
